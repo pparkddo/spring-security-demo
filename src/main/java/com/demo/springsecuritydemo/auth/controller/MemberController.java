9348179cbac44a5f5c57459aa84df479148affef
pparkddo <ehdud3453@gmail.com>
2021-11-17 02:15:19 +0900

    Fix: Change MemberController path

--- src/main/java/com/demo/springsecuritydemo/auth/controller/MemberController.java
+++ src/main/java/com/demo/springsecuritydemo/auth/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/member")
 @PreAuthorize("hasRole('ROLE_USER')")
 @RequiredArgsConstructor
 public class MemberController {
